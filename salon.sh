d46aa741969df6a99a18e595fce809fad4350ec7
Showan <44385680+showansimkhada@users.noreply.github.com>
2023-04-28 17:32:45 +1200

    files added

---
+++ salon.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+PSQL="psql -X --username=freecodecamp --dbname=salon --tuples-only -c"
+
+# main menu
+MAIN_MENU() {
+  if [[ ! $! ]]
+  then
+    echo -e "\n$1"
+  fi
+  echo -e "\nWhat can I do for you today?\n"
+  SERVICE_MENU=$($PSQL "SELECT service_id, name FROM services")
+  echo "$SERVICE_MENU" | while read SERVICE_ID BAR NAME
+  do
+    echo "$SERVICE_ID) $NAME"
+  done
+  read SERVICE_ID_SELECTED
+  SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id = $SERVICE_ID_SELECTED")
+  if [[ ! $SERVICE_ID_SELECTED =~ ^[1-3]$ ]] 
+  then
+    # send to main menu
+    MAIN_MENU "Please enter a valid service type."
+  else
+    echo -e "\nWhat's your phone number?"
+    read CUSTOMER_PHONE
+    CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+    if [[ -z $CUSTOMER_NAME ]]
+    then
+      echo -e "\nWhat's your name?"
+      read CUSTOMER_NAME
+      INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(name, phone) VALUES('$CUSTOMER_NAME','$CUSTOMER_PHONE')")
+      CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+      echo -e "\nEnter your preffered time: "
+      read SERVICE_TIME
+      INSERT_APPOINTMENT_RESULT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
+      echo -e "I have put you down for a $SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME."
+    else
+      CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+      echo -e "\nEnter your preffered time: "
+      read SERVICE_TIME
+      INSERT_APPOINTMENT_RESULT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
+      echo -e "I have put you down for a $SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME."
+    fi
+  fi
+}
+
+MAIN_MENU
